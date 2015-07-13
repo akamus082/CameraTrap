3f043db0bc8c377710824cb6b07e86e65c15ca61
akamus082 <akamus82@yahoo.com>
2015-07-13 11:34:29 -0700

    new additions

--- Algos/diffaccweight.py
+++ Algos/diffaccweight.py
@@ -17,7 +17,7 @@ t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 
 f = t.copy()
-
+gray = t.copy()
 avg = np.float32(f)
 
 # Read three images first:
@@ -26,6 +26,19 @@ avg = np.float32(f)
 while(1):
     #_,f = c.read()
   #img = f.copy()
+  mask = np.zeros(t.shape, dtype=np.uint8)
+  height, width = t.shape
+  cv2.circle(t, (312,262), 62, (0,0,0), -1)
+  cv2.circle(mask, (312,262), 160, (255,255,255), -1)
+
+  t = t & mask
+
+
+
+
+
+
+
   f = cv2.GaussianBlur(f,(5,5),0)
   cv2.accumulateWeighted(f,avg,0.4)
   res = cv2.convertScaleAbs(avg)
@@ -44,6 +57,18 @@ while(1):
     # Get the largest contour
     contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    '''for cnt in contours:
+        cv2.drawContours(zeros, [cnt], 0,255,-1)
+'''
+    #gray = cv2.bitwise_not(t)
+    #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    #res = cv2.morphologyEx(gray,cv2.MORPH_OPEN,kernel)
+    
+
+
+
+
+    
         # totally not from stack overflow
     areas = [cv2.contourArea(c) for c in contours]
     i_max  = np.argmax(areas)
@@ -56,16 +81,18 @@ while(1):
       #print "img_out.shape = " + str(img_out.shape)
       x_pos, y_pos = ctcv.getCentroid(contours[max_index])
       cv2.circle(t, (x_pos, y_pos), 5, (0,0,0), -1)
+    
+      x_pos, y_pos = ctcv.getCentroid(ctcv.contours[max_index])
+      theta = ctcv.getPolar(312,262, x_pos, y_pos)
+      print theta
 
 
 
 
-
-  cv2.imshow('image1',img_grey2)
-  cv2.imshow('image2',img_thresh)
+  #cv2.imshow('mask',mask)
   cv2.imshow('original',t)
-
-
+  #cv2.imshow('img_thresh',img_thresh)
+  
   
   t_minus = t
   t = t_plus

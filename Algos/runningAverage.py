616ae9376b5206018896e7ec35bb3101b61e3084
akamus082 <akamus82@yahoo.com>
2015-07-16 13:51:30 -0700

    some changes to running avg

--- Algos/runningAverage.py
+++ Algos/runningAverage.py
@@ -12,12 +12,12 @@ def lineSize(x1,y1,x2,y2):
 cam = cv2.VideoCapture(0)
 path = ('output.avi')
 
-cam.set(1, 10.0) #Match fps
+cam.set(1, 20.0) #Match fps
 cam.set(3,640)   #Match width
 cam.set(4,480)   #Match height
 
 fourcc = cv2.cv.CV_FOURCC(*'DIV3')
-video_writer = cv2.VideoWriter(path,fourcc, 10.0, (640,480))
+video_writer = cv2.VideoWriter(path,fourcc, 20.0, (640,480))
 
 winName = "1"
 got_frame, frame = cam.read()
@@ -65,22 +65,20 @@ while True:
   areas = [cv2.contourArea(c) for c in contour]
   max_index = np.argmax(areas)
   cnt=contour[max_index]
-
-  polygon_points = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
-  if (polygon_points.size == 4):
-    r = lineSize(polygon_points[0][0][0],polygon_points[0][0][1], polygon_points[1][0][0], polygon_points[1][0][1] )
-    if (r > 10000):
-      bounding_rect = cv2.boundingRect( cnt )
-      point1 = ( bounding_rect[0], bounding_rect[1] )
-      point2 = ( bounding_rect[0] + bounding_rect[2], bounding_rect[1] + bounding_rect[3] )
-      bounding_box_list.append( ( point1, point2 ) )
-      cv2.fillPoly( img_grey, [ polygon_points ], (255,255,255) )
-      cv2.polylines( display_image, [ polygon_points, ], 0, (255,255,255), 1, 0, 0 )
-      x,y,w,h = cv2.boundingRect(cnt)
-      cv2.rectangle(display_image,(x,y),(x+w,y+h),(0,255,0),2)
-      video_writer.write(frame)
+  if (cv2.contourArea(contour[max_index]) > 5000):
+    polygon_points = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+    bounding_rect = cv2.boundingRect( cnt )
+    point1 = ( bounding_rect[0], bounding_rect[1] )
+    point2 = ( bounding_rect[0] + bounding_rect[2], bounding_rect[1] + bounding_rect[3] )
+    bounding_box_list.append( ( point1, point2 ) )
+    cv2.fillPoly( img_grey, [ polygon_points ], (255,255,255) )
+    cv2.polylines( display_image, [ polygon_points, ], 0, (255,255,255), 1, 0, 0 )
+    x,y,w,h = cv2.boundingRect(cnt)
+    cv2.rectangle(display_image,(x,y),(x+w,y+h),(0,255,0),2)
+    video_writer.write(frame)
 
   cv2.imshow( winName[0], display_image )
+  
   key = cv2.waitKey(10)
   if key == 27:
     cv2.destroyWindow(winName)
